20016be79da920c4af06facc5444467f56a78b37
Octavio Berruti <octavioberruti@icloud.com>
2022-10-28 11:04:40 -0300

    ui consola

---
+++ tp2net/UI.Consola/Cursos.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Consola
+{
+    public class Cursos
+    {
+        public Cursos()
+        {
+            this.CursoNegocio = new CursoLogic();
+        }
+
+
+        protected CursoLogic _CursoNegocio;
+        public CursoLogic CursoNegocio
+        {
+            get { return _CursoNegocio; }
+            set { _CursoNegocio = value; }
+        }
+
+
+        public void Menu()
+        {
+            bool continua = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(
+                    "----------MENU----------\n" +
+                    "1 - Listado General.\n" +
+                    "2 - Consulta.\n" +
+                    "3 - Agregar.\n" +
+                    "4 - Modificar.\n" +
+                    "5 - Eliminar.\n" +
+                    "6 - Salir.\n");
+                string op = Console.ReadLine();
+                switch (op)
+                {
+                    case "1": ListadoGeneral(); break;
+                    case "2": Consultar(); break;
+                    case "3": Agregar(); break;
+                    case "4": Modificar(); break;
+                    case "5": Eliminar(); break;
+                    case "6": continua = false; break;
+                    default: break;
+                }
+                Console.ReadLine();
+            } while (continua == true);
+        }
+
+        public void ListadoGeneral()
+        {
+            Console.Clear();
+            foreach (Curso cur in CursoNegocio.GetAll())
+            {
+                MostrarDatos(cur);
+            }
+        }
+
+        public void MostrarDatos(Curso cur)
+        {
+            Console.WriteLine("Curso: {0}", cur.ID);
+            Console.WriteLine("\t\tNombre: {0}", cur.Desc_curso);
+            Console.WriteLine("\t\tApellido: {0}", cur.Año_calendario);
+            Console.WriteLine("\t\tNombre de Curso: {0}", cur.Cupo);
+
+            Console.WriteLine();
+        }
+
+        public void Consultar()
+        {
+            try
+            {
+                Console.Clear();
+                Console.Write("Ingrese el ID del Curso a consultar: ");
+                int id = int.Parse(Console.ReadLine());
+                this.MostrarDatos(CursoNegocio.GetOne(id));
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El ID ingresado debe ser un número entero.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Presione una tecla para continuar");
+            }
+        }
+
+        public void Modificar()
+        {
+            try
+            {
+                Console.Clear();
+                Console.Write("Ingrese el ID del Curso a modificar: ");
+                int id = int.Parse(Console.ReadLine());
+                Curso cur = CursoNegocio.GetOne(id);
+                Console.Write("Ingrese Descripcion: ");
+                cur.Desc_curso = Console.ReadLine();
+                Console.Write("Ingrese Año: ");
+                cur.Año_calendario = Console.ReadLine();
+                Console.Write("Ingrese cupo");
+                cur.Cupo = Console.ReadLine();
+                cur.State = BusinessEntity.States.Modified;
+                CursoNegocio.Save(cur);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El ID ingresado debe ser un número entero.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Presione una tecla para continuar");
+            }
+        }
+
+        public void Agregar()
+        {
+            Curso cur = new Curso();
+
+            Console.Clear();
+            Console.Write("Ingrese descripcion: ");
+            cur.Desc_curso = Console.ReadLine();
+            Console.Write("Ingrese año : ");
+            cur.Año_calendario = Console.ReadLine();
+            Console.Write("Ingrese cupo");
+            cur.Cupo = Console.ReadLine();
+            cur.State = BusinessEntity.States.New;
+            CursoNegocio.Save(cur);
+            Console.WriteLine();
+            Console.WriteLine("ID: {0}", cur.ID);
+        }
+
+        public void Eliminar()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese el ID del Curso a eliminar: ");
+                int id = int.Parse(Console.ReadLine());
+                CursoNegocio.Delete(id);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El ID ingresado debe ser un número entero.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Presione una tecla para continuar.");
+            }
+        }
+    }
+}
---
+++ tp2net/UI.Consola/Usuarios.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Consola
+{
+    public class Usuarios
+    {
+        public Usuarios()
+        {
+            this.UsuarioNegocio = new UsuarioLogic();
+        }
+
+
+        protected UsuarioLogic _UsuarioNegocio;
+        public UsuarioLogic UsuarioNegocio
+        {
+            get { return _UsuarioNegocio; }
+            set { _UsuarioNegocio = value; }
+        }
+
+
+        public void Menu()
+        {
+            bool continua = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(
+                    "----------MENU----------\n" +
+                    "1 - Listado General.\n" +
+                    "2 - Consulta.\n" +
+                    "3 - Agregar.\n" +
+                    "4 - Modificar.\n" +
+                    "5 - Eliminar.\n" +
+                    "6 - Salir.\n");
+                string op = Console.ReadLine();
+                switch (op)
+                {
+                    case "1": ListadoGeneral(); break;
+                    case "2": Consultar(); break;
+                    case "3": Agregar(); break;
+                    case "4": Modificar(); break;
+                    case "5": Eliminar(); break;
+                    case "6": continua = false; break;
+                    default: break;
+                }
+                Console.ReadLine();
+            } while (continua == true);
+        }
+
+        public void ListadoGeneral()
+        {
+            Console.Clear();
+            foreach (Usuario usr in UsuarioNegocio.GetAll())
+            {
+                MostrarDatos(usr);
+            }
+        }
+
+        public void MostrarDatos(Usuario usr)
+        {
+            Console.WriteLine("Usuario: {0}", usr.ID);
+            Console.WriteLine("\t\tNombre: {0}", usr.Nombre);
+            Console.WriteLine("\t\tApellido: {0}", usr.Apellido);
+            Console.WriteLine("\t\tNombre de Usuario: {0}", usr.NombreUsuario);
+            Console.WriteLine("\t\tClave: {0}", usr.Clave);
+            Console.WriteLine("\t\tEmail: {0}", usr.EMail);
+            Console.WriteLine("\t\tHabilitado: {0}", usr.Habilitado);
+            Console.WriteLine();
+        }
+
+        public void Consultar()
+        {
+            try
+            {
+                Console.Clear();
+                Console.Write("Ingrese el ID del usuario a consultar: ");
+                int id = int.Parse(Console.ReadLine());
+                this.MostrarDatos(UsuarioNegocio.GetOne(id));
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El ID ingresado debe ser un número entero.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Presione una tecla para continuar");
+            }
+        }
+
+        public void Modificar()
+        {
+            try
+            {
+                Console.Clear();
+                Console.Write("Ingrese el ID del usuario a modificar: ");
+                int id = int.Parse(Console.ReadLine());
+                Usuario usuario = UsuarioNegocio.GetOne(id);
+                Console.Write("Ingrese nombre: ");
+                usuario.Nombre = Console.ReadLine();
+                Console.Write("Ingrese apellido: ");
+                usuario.Apellido = Console.ReadLine();
+                Console.Write("Ingrese nombre de usuario");
+                usuario.NombreUsuario = Console.ReadLine();
+                Console.Write("Ingrese clave: ");
+                usuario.Clave = Console.ReadLine();
+                Console.Write("Ingrese Email: ");
+                usuario.EMail = Console.ReadLine();
+                Console.Write("Ingrese habilitacion de usuario (1-Si / otro-No): ");
+                usuario.Habilitado = (Console.ReadLine() == "1");
+                usuario.State = BusinessEntity.States.Modified;
+                UsuarioNegocio.Save(usuario);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El ID ingresado debe ser un número entero.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Presione una tecla para continuar");
+            }
+        }
+
+        public void Agregar()
+        {
+            Usuario usuario = new Usuario();
+
+            Console.Clear();
+            Console.Write("Ingrese nombre: ");
+            usuario.Nombre = Console.ReadLine();
+            Console.Write("Ingrese apellido: ");
+            usuario.Apellido = Console.ReadLine();
+            Console.Write("Ingrese nombre de usuario");
+            usuario.NombreUsuario = Console.ReadLine();
+            Console.Write("Ingrese clave: ");
+            usuario.Clave = Console.ReadLine();
+            Console.Write("Ingrese Email: ");
+            usuario.EMail = Console.ReadLine();
+            Console.Write("Ingrese habilitacion de usuario (1-Si / otro-No): ");
+            usuario.Habilitado = (Console.ReadLine() == "1");
+            usuario.State = BusinessEntity.States.New;
+            UsuarioNegocio.Save(usuario);
+            Console.WriteLine();
+            Console.WriteLine("ID: {0}", usuario.ID);
+        }
+
+        public void Eliminar()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese el ID del usuario a eliminar: ");
+                int id = int.Parse(Console.ReadLine());
+                UsuarioNegocio.Delete(id);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El ID ingresado debe ser un número entero.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Presione una tecla para continuar.");
+            }
+        }
+    }
+}
