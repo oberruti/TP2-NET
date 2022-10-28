216a8b61faf94833e067b3ca65a446d93d648a23
Octavio Berruti <octavioberruti@icloud.com>
2022-10-28 11:19:58 -0300

    web some files 2

---
+++ tp2net/UI.Web/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UI.Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUsuario_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Usuarios.aspx");
+        }
+
+        protected void btnMaterias_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Materias.aspx");
+        }
+
+        protected void btnComisiones_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Comisiones.aspx");
+        }
+
+        protected void btnCursos_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Cursos.aspx");
+        }
+    }
+}
