fa98d61e87aee6e2ee7ed5478f894f8f29186b06
Octavio Berruti <octavioberruti@icloud.com>
2022-10-28 11:18:34 -0300

    web some files

---
+++ tp2net/UI.Web/Comisiones.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Comisiones.aspx.cs" Inherits="UI.Web.Comisiones" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Panel ID="gridPanel" runat="server" AutoHeight="true">
+            <asp:GridView ID="gridView" runat="server" AutoGenerateColumns="False"
+                SelectedRowStyle-BackColor="Black"
+                SelectedRowStyle-ForeColor="White"
+                DataKeyNames="ID" Height="21px" OnSelectedIndexChanged="GridView_SelectedIndexChanged" Width="674px" >
+                <Columns>
+                    <asp:BoundField HeaderText="Descripcion" DataField="desc_comision" />
+                    <asp:BoundField HeaderText="Año Especialidad" DataField="anio_especialidad" />
+                    <asp:BoundField HeaderText="ID Plan" DataField="id_plan" />
+                    <asp:CommandField SelectText="Seleccionar" ShowSelectButton="true" />
+                </Columns>
+            </asp:GridView>
+        </asp:Panel>
+        <asp:Panel ID="formPanel" Visible="false" runat="server" Height="93px">
+            <asp:Label ID="desc_comisionLabel" runat="server" Text="Descripcion: "></asp:Label>
+            <asp:TextBox ID="desc_comisionTextBox" runat="server" ValidateRequestMode="Disabled" ValidationGroup="Validation"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="desc_comisionValidator" ControlToValidate="desc_comisionTextBox" ErrorMessage="Debe ingresar una descripcion." Display="Static" InitialValue="" Width="1%" runat="server" ForeColor="Red">*</asp:RequiredFieldValidator>
+            <br />
+            <asp:Label ID="anio_especialidadLabel" runat="server" Text="Año de Especialidad: "></asp:Label>
+            <asp:TextBox ID="anio_especialidadTextBox" runat="server" ValidateRequestMode="Disabled" ValidationGroup="Validation"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="anio_especialidadValidator" ControlToValidate="anio_especialidadTextBox" ErrorMessage="Debe ingresar el año de especialidad." Display="Static" InitialValue="" Width="1%" runat="server" ForeColor="Red">*</asp:RequiredFieldValidator>
+            <br />
+            <asp:Label ID="id_planLabel" runat="server" Text="ID de Plan: "></asp:Label>
+            <asp:TextBox ID="id_planTextBox" runat="server" ValidateRequestMode="Disabled" ValidationGroup="Validation"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="id_planValidator" ControlToValidate="id_planTextBox" ErrorMessage="Debe ingresar un ID de plan." Display="Static" InitialValue="" Width="1%" runat="server" ForeColor="Red">*</asp:RequiredFieldValidator>
+            <br />
+        </asp:Panel>
+        <asp:Panel ID="gridActionsPanel" runat="server" Height="85px">
+            <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click">Nuevo</asp:LinkButton>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:LinkButton ID="editarLinkButton" runat="server" OnClick="editarLinkButton_Click">Editar</asp:LinkButton>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click">Eliminar</asp:LinkButton>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="btnMenuPrincipal" runat="server" Text="Menu Principal" Width="167px" OnClick="btnMenuPrincipal_Click" />
+        </asp:Panel>
+        <asp:Panel ID="formActionsPanel" runat="server" Height="36px">
+            <asp:LinkButton ID="aceptarLinkButton" runat="server" OnClick="aceptarLinkButton_Click">Aceptar</asp:LinkButton>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:LinkButton ID="cancelarLinkButton" runat="server" OnClick="cancelarLinkButton_Click" CausesValidation="False">Cancelar</asp:LinkButton>
+            <br />
+            <br />
+        </asp:Panel>
+        <asp:ValidationSummary ID="ValidationFormPanel" runat="server" ForeColor="Red" />
+    </form>
+</body>
+</html>
---
+++ tp2net/UI.Web/Comisiones.aspx.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Web
+{
+    public partial class Comisiones : System.Web.UI.Page
+    {
+        private Comision Entity
+        {
+            get;
+            set;
+        }
+
+        ComisionLogic _logic;
+        private ComisionLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new ComisionLogic();
+                }
+                return _logic;
+            }
+        }
+
+        private int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+
+        private bool IsEntitySelected
+        {
+            get
+            {
+                return (this.SelectedID != 0);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.modo_vista(true);
+            LoadGrid();
+        }
+
+        public enum FormModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+
+        public FormModes FormMode
+        {
+            get { return (FormModes)this.ViewState["FormMode"]; }
+            set { this.ViewState["FormMode"] = value; }
+        }
+
+        protected void LoadGrid()
+        {
+            this.gridView.DataSource = this.Logic.GetAll();
+            this.gridView.DataBind();
+        }
+
+        protected void GridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.desc_comisionTextBox.Text = this.Entity.Desc_comision;
+            this.anio_especialidadTextBox.Text = this.Entity.Anio_especialidad.ToString();
+            this.id_planTextBox.Text = this.Entity.Id_Plan.ToString();
+        }
+
+        private void LoadEntity(Comision comision)
+        {
+            comision.Desc_comision = this.desc_comisionTextBox.Text;
+            comision.Anio_especialidad = int.Parse(this.anio_especialidadTextBox.Text);
+            comision.Id_Plan = int.Parse(this.id_planTextBox.Text);
+        }
+
+        private void SaveEntity(Comision comision)
+        {
+            try
+            {
+                this.Logic.Save(comision);
+            }
+            catch
+            {
+                Response.Write("<script>alert('Error: La comision no se ha guardado, por favor verifique los valores ingresados.')</script>");
+            }
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        private void EnableForm(bool enable)
+        {
+            this.desc_comisionTextBox.Enabled = enable;
+            this.anio_especialidadTextBox.Enabled = enable;
+            this.id_planTextBox.Enabled = enable;
+        }
+
+        // Vacia formulario luego de edicion
+        private void ClearForm()
+        {
+            this.desc_comisionTextBox.Text = string.Empty;
+            this.anio_especialidadTextBox.Text = string.Empty;
+            this.id_planTextBox.Text = string.Empty;
+        }
+
+        // BOTONES GRID
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.modo_vista(false);
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.modo_vista(false);
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.modo_vista(false);
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        // BOTONES FORM
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Alta:
+                    this.Entity = new Comision();
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    break;
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Comision();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    break;
+                default:
+                    break;
+            }
+            this.modo_vista(true);
+            this.LoadGrid();
+        }
+
+        protected void cancelarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.modo_vista(true);
+        }
+
+        // Otros metodos
+        protected void modo_vista(bool modo)
+        {
+            // true: seleccion - false: edicion.
+            this.gridPanel.Visible = modo;
+            this.formPanel.Visible = !modo;
+            this.gridActionsPanel.Visible = modo;
+            this.formActionsPanel.Visible = !modo;
+        }
+
+        protected void btnMenuPrincipal_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Default.aspx");
+        }
+    }
+}
