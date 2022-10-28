4fad79584529fade87d061252310be0adbf9e858
Octavio Berruti <octavioberruti@icloud.com>
2022-10-28 11:03:34 -0300

    logic

---
+++ tp2net/Business.Logic/AlumnoLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+using static Business.Entities.BusinessEntity;
+
+namespace Business.Logic
+{
+    public class AlumnoLogic : BusinessLogic
+    {
+        public AlumnoLogic()
+        {
+            this.AlumnoData = new AlumnoAdapter();
+        }
+
+        private AlumnoAdapter _AlumnoData;
+        public AlumnoAdapter AlumnoData
+        {
+            get { return _AlumnoData; }
+            set { _AlumnoData = value; }
+        }
+
+        public List<AlumnoInscripcion> GetAll()
+        {
+            return this.AlumnoData.GetAll();
+        }
+
+        public Business.Entities.AlumnoInscripcion GetOne(int ID)
+        {
+            return this.AlumnoData.GetOne(ID);
+        }
+
+        public void Delete(int ID)
+        {
+            this.AlumnoData.Delete(ID);
+        }
+
+        public void Save(AlumnoInscripcion alumno)
+        {
+            this.AlumnoData.Save(alumno);
+        }
+    }
+}
